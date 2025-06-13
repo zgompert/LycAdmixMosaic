ec334f4ec643950a4436db4a7f960eb9bb7ffce5
Zach Gompert <31893662+zgompert@users.noreply.github.com>
2025-06-13 15:14:43 -0600

    Add files via upload

--- GetSNPSubsetMax.R
+++ GetSNPSubsetMax.R
@@ -3,18 +3,16 @@
 
 library(data.table)
 
-ids<-read.table("../GenData/IDs.txt",header=FALSE)
 miss<-vector("list",23)
-reps<-grep(pattern="rep",ids[,1])
 for(i in 1:23){
 	ifile<-paste("text_max_chrom",i,".fasta",sep="")
 	dat<-fread(ifile,header=FALSE)
-	miss[[i]]<-apply(dat[-reps,]==5,2,mean)
+	miss[[i]]<-apply(dat==5,2,mean)
 
 }
-## retain 0.025 percent with the appropriate conditions
+## retain 0.035 percent with the appropriate conditions
 keepSNPs<-vector("list",23)
-prop<-0.00025
+prop<-0.00035
 for(i in 1:23){
 	xx<-which(miss[[i]]==0) ## no missing data
 	keepSNPs[[i]]<-sort(sample(xx,floor(length(miss[[i]])*prop),replace=FALSE))
